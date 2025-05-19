818bd5c821e29431b4dc8d8213989862b2012b19
Joseph Aldridge <joseph.aldridge@libtax.com>
2025-05-19 10:55:23 -0500

    chore: trigger frontend redeploy with Firebase config

--- client/src/config/firebase.ts
+++ client/src/config/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// Firebase configuration for LTS Recruiting
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,

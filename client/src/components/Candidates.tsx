6cb9d6eb8f801ab3c03cf5c16f8dcdd0f38b21c6
Joseph Aldridge <joseph.aldridge@libtax.com>
2025-06-05 15:00:29 -0500

    UI: Allow typing in the Autocomplete component for candidate search

--- client/src/components/Candidates.tsx
+++ client/src/components/Candidates.tsx
@@ -213,6 +213,7 @@ const Candidates: React.FC<CandidatesProps> = ({ user }) => {
           }}
           renderInput={(params) => <TextField {...params} label="Search Candidates" variant="outlined" />}
           loading={loading}
+          freeSolo
           sx={{ width: 300 }}
         />
         <TextField
